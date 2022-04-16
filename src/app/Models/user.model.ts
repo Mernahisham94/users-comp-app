0bf438f201cde9cff64d198e2361d10860c4714a
Mernahisham94 <79450024+Mernahisham94@users.noreply.github.com>
2022-04-16 15:41:26 +0200

    vvvv

---
+++ src/app/Models/user.model.ts
@@ -0,0 +1,5 @@
+export class User {
+    name:string="";
+    code:string="";
+    imagePath:string="";
+}
---
+++ src/app/users/users.component.ts
@@ -0,0 +1,62 @@
+import { Component, OnInit } from '@angular/core';
+import  {User}  from '../Models/user.model'; 
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-users',
+  templateUrl: './users.component.html',
+  styleUrls: ['./users.component.css']
+})
+export class UsersComponent implements OnInit {
+  DataPath: string =  'assets/Data/users.json';  
+  users: User[] = [
+    {
+    "name": "ahmed",
+    "code": "0001",
+    "imagePath": "user.png"
+    },{
+    "name": "mohamed",
+    "code": "0001.0003.0004.0002",
+    "imagePath": "user.png"
+    },{
+    "name": "ali",
+    "code": "0001.0003",
+    "imagePath": "user.png"
+    },{
+    "name": "ibrahim",
+    "code": "0001.0003.0004",
+    "imagePath": "user.png"
+    },{
+    "name": "mahmoud",
+    "code": "0001.0010.0005",
+    "imagePath": "user.png"
+    },{
+    "name": "samy",
+    "code": "0001.0010.0006",
+    "imagePath": "user.png"
+    },{
+    "name": "hany",
+    "code": "0001.0003.0004.0007",
+    "imagePath": "user.png"
+    },{
+    "name": "samir",
+    "code": "0001.0010.0005.0008",
+    "imagePath": "user.png"
+    },{
+    "name": "khalid",
+    "code": "0001.0010.0006.0009",
+    "imagePath": "user.png"
+    },{
+    "name": "abdullah",
+    "code": "0001.0010",
+    "imagePath": "user.png"
+    }
+    ];  
+  constructor( ){ }
+
+  ngOnInit(): void {
+    
+  }
+
+
+}
